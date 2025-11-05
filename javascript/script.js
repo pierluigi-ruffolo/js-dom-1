7a6c265f0f3cc246672fb89b9b28450222bcd05d
pierluigi-ruffolo <pierluigiruffolo1@gmail.com>
2025-11-05 15:00:01 +0100

    feat: aggiunta logica  per accendere e spegnere la lampadina

--- javascript/script.js
+++ javascript/script.js
@@ -7,3 +7,46 @@ trovate in allegato e accanto un bottone con la scritta “Accendi”. */
 
 /* BONUS
 Facciamo accendi e spegni: */
+/* Al primo click la lampadina si accende e nel bottone 
+compare la scritta "Spegni"
+Al secondo click la lampadina si spegne e nel bottone
+ compare la scritta "Accendi"
+E così via...
+Rendete il tutto un po più bello e creativo  */
+
+/* raccolgo elementi necessari */
+const imgElem = document.querySelector(".img");
+console.log(imgElem);
+const btnElem = document.querySelector(".btn");
+console.log(btnElem);
+
+/* uso arrow function */
+
+/* btnElem.addEventListener("click", () => {
+  if (btnElem.innerHTML === "Spegni") {
+    imgElem.src = "./img/white_lamp.png";
+    btnElem.innerHTML = "Accendi";
+    btnElem.classList.remove("btn-danger");
+  } else {
+    imgElem.src = "./img/yellow_lamp.png";
+    btnElem.innerHTML = "Spegni";
+    btnElem.classList.add("btn-danger");
+  }
+}); */
+
+/* ALTRA VERSIONE */
+let valore = true;
+
+btnElem.addEventListener("click", () => {
+  if (valore) {
+    imgElem.src = "./img/yellow_lamp.png";
+    btnElem.innerHTML = "Spegni";
+    btnElem.classList.add("btn-danger");
+  } else {
+    imgElem.src = "./img/white_lamp.png";
+    btnElem.innerHTML = "Accendi";
+    btnElem.classList.remove("btn-danger");
+  }
+
+  valore = !valore;
+});
